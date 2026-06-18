e29a16ee1b47afab0ba7e2b052c3b5aa0b3bc6a6
Azim Utkirovich <azimutkirovich80@gmail.com>
2026-06-18 19:48:57 +0500

    Optimize headers initialization in api.ts

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -1,5 +1,5 @@
 const apiKey = import.meta.env.VITE_API_KEY;
-const headers: HeadersInit = {}
+const headers: Record<string, string> = apiKey ? { "api-key": apiKey } : {};
 
 if (apiKey) {
   headers['api-key'] = apiKey;
@@ -8,18 +8,17 @@ if (apiKey) {
 export const getTrack = (trackId: string) => {
   return fetch(
     "https://musicfun.it-incubator.app/api/1.0/playlists/tracks/" + trackId,
-    {
-      headers: headers,
-    },
-  ).then((res) => res.json());
+    { headers: headers },
+  ).then((res) => {
+    if (!res.ok) throw new Error("Network response was not ok");
+    return res.json();
+  });
 };
 
 export const getTracks = () => {
   return fetch(
     "https://musicfun.it-incubator.app/api/1.0/playlists/tracks?pageSize=5",
-    {
-      headers: headers,
-    },
+    { headers: headers },
   ).then((res) => {
     if (!res.ok) throw new Error("Network response was not ok");
     return res.json();
